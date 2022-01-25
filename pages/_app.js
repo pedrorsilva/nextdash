75bff0168412f432757b011c38cc60ae23393260
Pedro <silva.pedror@gmail.com>
2022-01-25 20:32:43 -0300

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,18 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// import "../styles/globals.css";
+import "../styles/style.css";
+
+import Head from "next/head";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+export default MyApp;
